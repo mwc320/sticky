d71c9821c1dd614286c10af39b3208dfb1be39c2
Michael Colgan <mcolgan@tryworkspace.com>
2018-04-17 23:52:16 -0400

    1

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const keyPublishable = 'pk_test_MFfbY7MJNdAfiej6EQMreG8w';
-const keySecret = 'sk_test_p6zoVCYMwWJ1youg50TCHesk';
+// const keySecret = 'sk_test_p6zoVCYMwWJ1youg50TCHesk';
 
 const express = require("express");
 const stripe = require("stripe")(keySecret);
@@ -11,23 +11,35 @@ app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
-app.post("/charge", (req, res) => {
+var stripe = require("stripe")(
+  "sk_test_p6zoVCYMwWJ1youg50TCHesk"
+);
+
+var customer = await stripe.customers.create(
+  { email: 'customer@example.com' }
+);
+
+app.post("https://api.stripe.com/charge/", (req, res) => {
   let amount = 1199;
 
   stripe.customers.create({
-     email: req.body.stripeEmail,
-    source: req.body.stripeToken
+    email: req.body.email,
+    card: req.body.id
   })
   .then(customer =>
     stripe.charges.create({
       amount,
       description: "One 10ml bottle - Shipped!",
-         currency: "usd",
-         customer: customer.id
+      currency: "usd",
+      customer: customer.id
     }))
-  .then(charge => res.render("charge.pug"));
+  .then(charge => res.send(charge))
+  .catch(err => {
+    console.log("Error:", err);
+    res.status(1199).send({error: "Purchase Failed"});
+  });
 });
 
-app.listen(4567);
+app.listen(8000);
 
 
